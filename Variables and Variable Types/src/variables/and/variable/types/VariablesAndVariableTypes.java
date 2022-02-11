3e600a7cc34b9912341d288ee579119f9fa0da49
Puniterz <99297488+Puniterz@users.noreply.github.com>
2022-02-11 14:23:21 -0500

    Add files via upload

---
+++ Variables and Variable Types/src/variables/and/variable/types/VariablesAndVariableTypes.java
@@ -0,0 +1,40 @@
+/*
+ * Punit Pabla.
+ * 2/8/2022
+ * variables.and.variable.types
+ *describes what i like and who i am
+*/
+package variables.and.variable.types;
+
+/**
+ *
+ * @author pupab0551
+ */
+public class VariablesAndVariableTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //variabls 
+        String fname;
+        String lname;
+        int age;
+        double height; 
+        boolean likec;
+        //diclaring variables 
+        fname = "Punit";
+        lname = "Pabla";
+        age = 16;
+        height = 179.07;
+        likec = true;
+        //output
+        System.out.println ("First Name: " + fname);
+        System.out.println ("Last Name: " + lname);
+        System.out.println ("Age: " + age);
+        System.out.println ("Height: " + height);
+        System.out.println ("Likes Computers?" + likec);
+        // TODO code application logic here
+    }
+    
+}
